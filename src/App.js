e2f9708c8f19eb2be8c7bf23523f1b35ee25e000
deepalitiwari <anjalisingh111978@mail.com>
2024-03-15 18:44:54 +0530

    Items added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import "./App.css";
-import Collection from "./Components/Browse_Our_Collection/Collection";
-import DesignDream from "./Components/Design_Dream/DesignDream";
-import Footer from "./Components/Footer/Footer";
+// import Collection from "./Components/Browse_Our_Collection/Collection";
+// import DesignDream from "./Components/Design_Dream/DesignDream";
+// import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
-import Main from "./Components/Main/Main";
+import Item from "./Components/Items/Item";
+// import Main from "./Components/Main/Main";
 import Menu from "./Components/Menu/Menu";
-import Strategy from "./Components/Strategy/Strategy";
-import { data } from "./data";
-
+// import Strategy from "./Components/Strategy/Strategy";
+// import { data } from "./data";
+import {
+  beddingBath,
+  beddingBathHead,
+  bottomArea,
+} from "./Components/Items/data1";
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Menu />
+      {/* <Main />
       <Menu />
       <Collection />
       {data.map(({ h3, src, children, direction, style, textColor }, index) => {
@@ -34,13 +40,13 @@ function App() {
         src="https://www.moooi.com/_next/image?url=https%3A%2F%2Fcdn.moooi.com%2Ftmp%2Fimage-thumbnails%2FWeb-Items%2FStories%2FPodcasts%2Fimage-thumb__62604__header_fullscreen_2x%2FPodcast_Design_Dreams_Mobile_Phone_Walking_In_Clouds.png&w=1080&q=80"
         direction="row-reverse"
         style="#4C363A;
-    
         "
       >
         Moooi Through the Eyes of Rebelo de Andrade Architecture
       </DesignDream>
 
-      <Footer />
+      <Footer /> */}
+      <Item data={beddingBath} dataHead={beddingBathHead} bottom={bottomArea} />
     </div>
   );
 }
---
+++ src/Components/Items/Item.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import "./Item.css";
+import { SlArrowLeftCircle } from "react-icons/sl";
+import { MdOutlineManageSearch } from "react-icons/md";
+
+function Item({ dataHead, data, bottom }) {
+  //   console.log("DATA", dataHead);
+  return (
+    <div className="items">
+      <div className="main">
+        <h1 className="heading">{dataHead.name}</h1>
+        <p className="para">
+          Our collection contains work from emerging talent and internationally
+          recognized designers. Together, we create A Life Extraordinary.
+        </p>
+        <div className="btn">
+          <div className="item-btn">
+            <SlArrowLeftCircle className="icon" />
+
+            {dataHead.Btn.map((item) => {
+              return <button>{item}</button>;
+            })}
+          </div>
+          <div className="filter-btn">
+            <button>
+              <MdOutlineManageSearch />
+              Filter
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="item-carts">
+        {data.map((item, index) => (
+          <div className="cart" key={index}>
+            <div className="cartImg">
+              <img
+                key={item.id}
+                src={item.src}
+                alt={item.alt}
+                title={item.title}
+              />
+            </div>
+            <div className="cartTitle">{item.title}</div>
+            <div className="cartPrice">{item.price}</div>
+            <div className="cartavailable">{item.available}</div>
+          </div>
+        ))}
+      </div>
+      {/* <div className="item-bottom">
+        <h1 className="heading">our suggestions</h1>
+        <p className="para">
+          Turn more dreams into reality. <br /> {bottom.p}
+        </p>
+        <div className="btn">
+          {bottom.bottomBtn.map((item) => {
+            return <button>{item}</button>;
+          })}
+        </div>
+      </div> */}
+    </div>
+  );
+}
+
+export default Item;
